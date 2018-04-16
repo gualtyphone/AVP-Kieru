3656c5b4e1498fc8b65ea348e44b0a66966582c4
josh <iownall99@live.co.uk>
2018-04-16 20:52:40 +0100

    MONDAYS work

---
+++ Pete Animations/Assets/DelayPlayableDirector.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class DelayPlayableDirector : MonoBehaviour {
+
+	[SerializeField]
+	float delay = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (delayPlayableDirector ());
+	}
+
+	IEnumerator delayPlayableDirector()
+	{
+		yield return new WaitForSeconds (delay);
+		GetComponent<PlayableDirector> ().Play ();
+		yield return null;
+	}
+}
